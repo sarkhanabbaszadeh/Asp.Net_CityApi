c57e03c82294baf6c11aaa4a045cfed503c3d855
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2023-02-22 11:58:50 +0400

    login api con. added, json file fixed

--- Asp.Net_CityApi/Controllers/AuthController.cs
+++ Asp.Net_CityApi/Controllers/AuthController.cs
@@ -1,8 +1,11 @@
 ﻿using Asp.Net_CityApi.Data;
 using Asp.Net_CityApi.Dtos;
 using Asp.Net_CityApi.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Asp.Net_CityApi.Controllers
 {
@@ -27,7 +30,7 @@ namespace Asp.Net_CityApi.Controllers
 				ModelState.AddModelError("UserName", "Username already exists");
 			}
 
-			if(ModelState.IsValid)
+			if(!ModelState.IsValid)
 			{
 				return BadRequest(ModelState);
 			}
@@ -39,6 +42,36 @@ namespace Asp.Net_CityApi.Controllers
 
 			var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
 			return StatusCode(201);
+		}
+
+		[HttpPost("login")]
+		public async Task<ActionResult> Login([FromBody] UserForLoginDto userForLoginDto)
+		{
+			var user= await _authRepository.Login(userForLoginDto.UserName, userForLoginDto.Password);
+
+			if(user == null)
+			{
+				return Unauthorized();
+			}
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Token").Value);
+
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+				{
+					new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+					new  Claim(ClaimTypes.Name,user.Username)
+				}),
+				Expires = DateTime.Now.AddDays(1),
+				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+			};
+
+			var token=tokenHandler.CreateToken(tokenDescriptor);
+			var tokenString=tokenHandler.WriteToken(token);
+
+			return Ok(token);
 		}
 	}
 }
