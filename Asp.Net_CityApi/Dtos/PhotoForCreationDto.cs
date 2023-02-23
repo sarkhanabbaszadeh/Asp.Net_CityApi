a163a573de627b54409f809b6cdb7ed40102482d
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2023-02-23 11:12:20 +0400

    added new dto for photo

---
+++ Asp.Net_CityApi/Dtos/PhotoForCreationDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Asp.Net_CityApi.Dtos
+{
+	public class PhotoForCreationDto
+	{
+        public PhotoForCreationDto()
+        {
+            DateAdded=DateTime.Now;
+        }
+        public string Url { get; set; }
+
+		public IFormFile File { get; set; }
+
+		public string Description { get; set; }
+
+		public DateTime DateAdded { get; set; }
+
+		public string PuplicId { get; set; }
+	}
+}
---
+++ Asp.Net_CityApi/Dtos/PhotoForReturnDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Asp.Net_CityApi.Dtos
+{
+	public class PhotoForReturnDto
+	{
+		public string Id { get; set; }
+
+		public string Url { get; set; }
+
+		public string Description { get; set; }
+
+		public DateTime DateAdded { get; set; }
+
+		public bool IsMain { get; set; }
+
+		public string PublicID { get; set; }
+
+	}
+}
